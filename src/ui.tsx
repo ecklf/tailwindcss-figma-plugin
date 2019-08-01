9e3b825bbeba9f00cc5ed0e3cd42f101faa8a6b1
impulse <ecklf@icloud.com>
2019-08-01 23:36:21 +0200

    add base style generator

--- src/ui.tsx
+++ src/ui.tsx
@@ -8,7 +8,7 @@ class App extends React.Component {
   textbox: HTMLInputElement;
 
   countRef = (element: HTMLInputElement) => {
-    if (element) element.value = "5";
+    if (element) element.value = "tw";
     this.textbox = element;
   };
 
@@ -24,29 +24,39 @@ class App extends React.Component {
     parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
   };
 
+  onTest = () => {
+    parent.postMessage({ pluginMessage: { type: "test" } }, "*");
+  };
+
   render() {
     return (
-      <div className="px-4 py-4 bg-gray-100">
+      <div className="px-4 py-2">
         {/* <img src={require("./logo.svg")} /> */}
-        <h2 className="text-bold text-xl">Rectangle Creator</h2>
+        <h2 className="text-bold text-gray-800 text-2xl">TailwindCSS Plugin</h2>
         <label className="block py-2">
-          <span className="text-gray-700">Count</span>
+          <span className="text-gray-700">Prefix</span>
           <input
             ref={this.countRef}
-            className="form-input my-4 block w-full"
-            placeholder="Number of rectangles"
+            className="form-input my-2 block w-full"
+            placeholder="prefix"
           />
         </label>
 
         <div className="flex flex-row-reverse items-center">
           <button
+            onClick={this.onTest}
+            className="ml-4 bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Generate
+          </button>
+          {/* <button
             onClick={this.onCreate}
             className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
           >
             Create
-          </button>
+          </button> */}
           <button
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            className="inline-block align-baseline font-bold text-sm text-gray-600 hover:text-gray-700"
             onClick={this.onCancel}
           >
             Cancel
