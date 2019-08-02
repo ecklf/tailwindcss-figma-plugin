2e338de53871ec7bbe2f10a6ed1509e1b2f4b2ce
impulse <ecklf@icloud.com>
2019-08-02 14:00:00 +0200

    refactor: change entry paths

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === "production" ? false : "inline-source-map",
 
   entry: {
-    ui: "./src/ui.tsx", // The entry point for your UI code
-    code: "./src/code.ts" // The entry point for your plugin code
+    ui: "./src/App.tsx", // The entry point for your UI code
+    code: "./src/init.ts" // The entry point for your plugin code
   },
 
   module: {
