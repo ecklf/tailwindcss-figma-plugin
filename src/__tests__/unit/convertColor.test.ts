b4f459f7146ecea90f5aa3c63f2b0f063922559f
impulse <ecklf@icloud.com>
2019-08-18 21:58:04 +0200

    test: add color tests

--- src/__tests__/unit/convertColor.test.ts
+++ src/__tests__/unit/convertColor.test.ts
@@ -3,6 +3,7 @@ import {convertColor} from '../../util/convertColor';
 const colorObject = {
   theme: {
     colors: {
+      transparent: 'transparent',
       hex: '#E53E3E',
       rgb: 'rgb(56,161,105)',
       rgba: 'rgba(49,151,149, 0.5)',
@@ -16,6 +17,24 @@ const {colors} = colorObject.theme;
 
 describe('Passing a valid config color', () => {
   let currentColor;
+
+  it('should return object from transparent string', () => {
+    currentColor = convertColor(colors['transparent']);
+    // Check if valid object
+    expect(currentColor).toHaveProperty('r');
+    expect(currentColor).toHaveProperty('g');
+    expect(currentColor).toHaveProperty('b');
+    expect(currentColor).toHaveProperty('a');
+    // Check if valid values
+    expect(currentColor['r']).toBeGreaterThanOrEqual(0);
+    expect(currentColor['r']).toBeLessThanOrEqual(1);
+    expect(currentColor['g']).toBeGreaterThanOrEqual(0);
+    expect(currentColor['g']).toBeLessThanOrEqual(1);
+    expect(currentColor['b']).toBeGreaterThanOrEqual(0);
+    expect(currentColor['b']).toBeLessThanOrEqual(1);
+    expect(currentColor['a']).toBeGreaterThanOrEqual(0);
+    expect(currentColor['a']).toBeLessThanOrEqual(1);
+  });
 
   it('should return object from hex', () => {
     currentColor = convertColor(colors['hex']);
