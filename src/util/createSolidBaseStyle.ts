7e73ca20966bbeaf200b85849a307a990bdbf7d1
impulse <ecklf@icloud.com>
2019-08-18 21:56:33 +0200

    refactor: delete actions

--- src/util/createSolidBaseStyle.ts
+++
@@ -1,8 +0,0 @@
-import { hexToRgb } from "./hexToRgb";
-
-export function createSolidBaseStyle(name, value) {
-  const style = figma.createPaintStyle();
-  style.name = name;
-  const color = hexToRgb(value);
-  style.paints = [{ type: "SOLID", color }];
-}
--- src/util/validateFonts.ts
+++
@@ -1,3 +0,0 @@
-export function validateFonts(fontList) {
-  console.log('hi');
-}
