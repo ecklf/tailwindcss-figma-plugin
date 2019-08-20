df58f458169af70fcc6add5c3905ca02596896ca
impulse <ecklf@icloud.com>
2019-08-20 19:56:21 +0200

    fix: fixes #9 - fix and improve js module evaluation

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,24 +1,30 @@
-const parse = require("parse-color");
+const parse = require('parse-color');
 
 export function parseConfig(config: string): object {
   try {
-    config = config.replace(/require\(.+?\)/g, "");
+    // Remove any whitespace
+    config = config.replace(/\s/g, '');
+    // Remove anything before module exports
+    config = config.replace(/^(.*)(?=module.exports)/gi, '');
+    // Remove plugins section
+    config = config.replace(/,plugins(.*?)(?!.*\])(?=})/gi, '');
     return eval(config);
   } catch (error) {
-    return {};
+    console.error(error);
+    throw new Error(error);
   }
 }
 
 export function convertConfigColor(color: string): RGBA {
-  if (color === "transparent") {
-    color = "rgba(0, 0, 0, 0.0)";
+  if (color === 'transparent') {
+    color = 'rgba(0, 0, 0, 0.0)';
   }
-  const { rgba } = parse(color);
+  const {rgba} = parse(color);
   return {
     r: rgba[0] / 255,
     g: rgba[1] / 255,
     b: rgba[2] / 255,
-    a: rgba[3]
+    a: rgba[3],
   };
 }
 
@@ -26,25 +32,33 @@ export const fetchConfigColors = (config): Array<TailwindColor> => {
   let foundColors: Array<TailwindColor> = [];
 
   if (config.theme) {
-    const { theme } = config;
+    const {theme} = config;
+    let mergedColors = {};
+
     if (theme.colors) {
-      // Looping through all color names
-      for (const color of Object.keys(theme.colors)) {
-        const colorData = theme.colors[color];
-        // Determine if the current color has nested shades
-        if (typeof colorData === "object") {
-          for (const colorShade of Object.keys(colorData)) {
-            foundColors.push({
-              name: `${color}/${colorShade}`,
-              value: convertConfigColor(colorData[colorShade])
-            });
-          }
-        } else {
+      mergedColors = {...theme.colors};
+    }
+
+    if (theme.extend && theme.extend.colors) {
+      mergedColors = {...theme.extend.colors};
+    }
+
+    // Looping through all overwritten and extended colors
+    for (const color of Object.keys(mergedColors)) {
+      const colorData = mergedColors[color];
+      // Determine if the current color has nested shades
+      if (typeof colorData === 'object') {
+        for (const colorShade of Object.keys(colorData)) {
           foundColors.push({
-            name: color,
-            value: convertConfigColor(theme.colors[color])
+            name: `${color}/${colorShade}`,
+            value: convertConfigColor(colorData[colorShade]),
           });
         }
+      } else {
+        foundColors.push({
+          name: color,
+          value: convertConfigColor(mergedColors[color]),
+        });
       }
     }
   }
