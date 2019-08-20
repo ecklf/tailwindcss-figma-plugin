dfa9b2237855797fc4067d5ff6847e07e67e2766
impulse <ecklf@icloud.com>
2019-08-20 22:09:04 +0200

    feat: adding dropzone for config upload

--- src/components/ColorView.tsx
+++ src/components/ColorView.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
-function ColorView({ twColors, onAddColors }) {
-  const [prefix, setPrefix] = useState("");
+function ColorView({twColors, onAddColors}) {
+  const [prefix, setPrefix] = useState('');
 
   return (
     <React.Fragment>
@@ -12,23 +12,20 @@ function ColorView({ twColors, onAddColors }) {
         <input
           value={prefix}
           onChange={e => setPrefix(e.target.value)}
-          className="mt-2 w-full form-input block"
+          className="focus:shadow-none select-none mt-2 w-full form-input block"
           placeholder="Style name prefix (optional)"
         />
       </div>
-
-      {/* <hr className="my-6" /> */}
 
       <div className="flex flex-row justify-between items-center">
         <button
           onClick={() => {
             onAddColors(prefix);
           }}
-          className={`mt-3 bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded ${
-            twColors.length === 0 ? "opacity-50 cursor-not-allowed" : ""
+          className={`select-none focus:outline-none mt-2 bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded ${
+            twColors.length === 0 ? 'opacity-50 cursor-not-allowed' : ''
           }`}
-          disabled={twColors.length === 0 ? true : false}
-        >
+          disabled={twColors.length === 0 ? true : false}>
           Add Styles
         </button>
       </div>
